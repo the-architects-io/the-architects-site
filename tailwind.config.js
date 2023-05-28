c0fbbe192611d167d65cf9a3f15631cbf0aab898
warly <warlyware@pm.me>
2023-05-27 17:48:58 -0700

    Add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          900: '#0f0f0f'
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
