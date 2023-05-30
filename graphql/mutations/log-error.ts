e7c248ca7ee47eba1fb4ad4fbf9be667e0b79a0a
warly <warlyware@pm.me>
2023-05-29 20:03:30 -0700

    Add missing files

---
+++ graphql/mutations/log-error.ts
@@ -0,0 +1,24 @@
+import { gql } from "@apollo/client";
+
+export const LOG_ERROR = gql`
+  mutation LOG_ERROR(
+    $code: Int!
+    $message: String!
+    $rawError: String!
+    $burnTxAddress: String = ""
+  ) {
+    insert_errorInstances_one(
+      object: {
+        code: $code
+        message: $message
+        rawError: $rawError
+        burnTxAddress: $burnTxAddress
+      }
+    ) {
+      code
+      id
+      message
+      rawError
+    }
+  }
+`;
