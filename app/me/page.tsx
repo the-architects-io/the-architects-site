0f6d5d73386d2280c1fec776f1b542ea747a5c93
warly <warlyware@pm.me>
2023-06-23 23:26:26 -0700

    Imprvove login flow

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -6,6 +6,7 @@ import Spinner from "@/features/UI/spinner";
 import {
   useAuthenticated,
   useAuthenticationStatus,
+  useProviderLink,
   useSignOut,
   useUserData,
 } from "@nhost/nextjs";
@@ -15,6 +16,8 @@ import { useEffect } from "react";
 
 export default function Page() {
   const { isAuthenticated, isLoading } = useAuthenticationStatus();
+  const { discord } = useProviderLink();
+
   const user = useUserData();
   const { signOut } = useSignOut();
   const router = useRouter();
@@ -45,7 +48,13 @@ export default function Page() {
       <Panel className="flex flex-col items-center mb-8 w-full">
         <h1 className="text-3xl font-bold mb-4">{displayName}</h1>
         <Image alt="avatar" src={user?.avatarUrl} width={30} height={30} />
-        <div className="mb-4">{JSON.stringify(user)}</div>
+        <div className="mb-4 break-all">{JSON.stringify(user)}</div>
+        <a
+          href={discord}
+          className="bg-purple-700 text-white p-2 px-4 rounded-lg mb-4 uppercase"
+        >
+          Sign in with Discord
+        </a>
         <PrimaryButton onClick={handleSignOut}>Sign out</PrimaryButton>
       </Panel>
     </ContentWrapper>
