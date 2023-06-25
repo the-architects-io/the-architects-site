0fd76dda7f30921b8807f911efec5a4b6eec8520
warly <warlyware@pm.me>
2023-06-24 17:50:47 -0700

    Remove unimplemented trait add fn

--- utils/nfts/add-traits-to-db.ts
+++ utils/nfts/add-traits-to-db.ts
@@ -1,43 +1,43 @@
-import client from "@/graphql/apollo/client";
 import { ADD_TRAIT } from "@/graphql/mutations/add-trait";
 import { GET_TRAITS_BY_NFT_COLLECTION } from "@/graphql/queries/get-traits-by-nft-collection";
 
 export const addTraitsToDb = async (nfts: any[], nftCollectionId: string) => {
-  const { data } = await client.query({
-    query: GET_TRAITS_BY_NFT_COLLECTION,
-    variables: {
-      nftCollectionId,
-    },
-  });
+  throw new Error("Not implemented");
+  // const { data } = await client.query({
+  //   query: GET_TRAITS_BY_NFT_COLLECTION,
+  //   variables: {
+  //     nftCollectionId,
+  //   },
+  // });
 
-  const { traits: traitsFromDb } = data;
+  // const { traits: traitsFromDb } = data;
 
-  let traitsFromNfts = nfts.map(({ traits }) => traits).flat();
-  const traitNamesFromNfts = traitsFromNfts.map(
-    (trait: { name: string }) => trait.name
-  );
-  const traitNamesFromDb = traitsFromDb.map(
-    (trait: { name: string }) => trait.name
-  );
+  // let traitsFromNfts = nfts.map(({ traits }) => traits).flat();
+  // const traitNamesFromNfts = traitsFromNfts.map(
+  //   (trait: { name: string }) => trait.name
+  // );
+  // const traitNamesFromDb = traitsFromDb.map(
+  //   (trait: { name: string }) => trait.name
+  // );
 
-  const traitNamesNotInDb = traitNamesFromNfts.filter(
-    (traitName) => !traitNamesFromDb.includes(traitName)
-  );
+  // const traitNamesNotInDb = traitNamesFromNfts.filter(
+  //   (traitName) => !traitNamesFromDb.includes(traitName)
+  // );
 
-  console.log({
-    traitNamesFromNfts,
-    traitNamesFromDb,
-    traitNamesNotInDb,
-  });
+  // console.log({
+  //   traitNamesFromNfts,
+  //   traitNamesFromDb,
+  //   traitNamesNotInDb,
+  // });
 
-  for (const name of traitNamesNotInDb) {
-    await client.mutate({
-      mutation: ADD_TRAIT,
-      variables: {
-        name,
-        nftCollectionId,
-      },
-    });
-    console.log(`Added trait ${name} to db`);
-  }
+  // for (const name of traitNamesNotInDb) {
+  //   await client.mutate({
+  //     mutation: ADD_TRAIT,
+  //     variables: {
+  //       name,
+  //       nftCollectionId,
+  //     },
+  //   });
+  //   console.log(`Added trait ${name} to db`);
+  // }
 };
